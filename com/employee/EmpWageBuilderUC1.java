5e47b751fb37484a5eab65ce5f752c68fa640aac
sks <sks123git@gmail.com>
2023-02-02 14:01:21 +0530

    added emp wage in a day

---
+++ com/employee/EmpWageBuilderUC1.java
@@ -0,0 +1,16 @@
+package com.employee;
+
+import java.util.Random;
+
+public class EmpWageBuilderUC1 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;//constants
+        double empCheck = Math.floor((Math.random() * 10) % 2);
+        System.out.println(empCheck);
+        if(IS_FULL_TIME==empCheck){
+            System.out.println("Employee is full time.");
+        }else {
+            System.out.println("Employee is absent.");
+        }
+    }
+}
---
+++ com/employee/EmpWageBuilderUC2.java
@@ -0,0 +1,19 @@
+package com.employee;
+
+public class EmpWageBuilderUC2 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;//constants
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        double empCheck = Math.floor((Math.random() * 10) % 2);
+        System.out.println(empCheck);
+        if(IS_FULL_TIME==empCheck){
+            empHrs = 8;
+        }else {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage: " + empWage);
+    }
+}
